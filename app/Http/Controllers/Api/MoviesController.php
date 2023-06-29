9dc1e5b915748b05b5ee7efb783d60c911b9c988
Samuel Alapakristi <samuel@onepipe.io>
2023-06-29 20:53:37 +0100

    chore

--- app/Http/Controllers/Api/MoviesController.php
+++ app/Http/Controllers/Api/MoviesController.php
@@ -3,13 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use Exception;
-use App\Models\Comment;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Services\MovieService;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Cache;
-use PhpParser\Node\Expr\Isset_;
 
 class MoviesController extends Controller
 {
