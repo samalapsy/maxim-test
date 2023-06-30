10357346e8a3b7fc9ca1a959b2c28cabd68dd8d8
Samuel Alapakristi <samuel@onepipe.io>
2023-06-30 18:23:59 +0100

    ft: added total height in cm and in ft

--- app/Http/Controllers/Api/GetMovieCharactersController.php
+++ app/Http/Controllers/Api/GetMovieCharactersController.php
@@ -20,10 +20,14 @@ class GetMovieCharactersController extends Controller
         try {
             $data = $movieService->getMovieCharacters($movie_id);
             $characters = $this->filterCharacters($data);
-            $total_height = 0;
+            $total_height = $characters?->map(fn ($x) => $x?->height)?->reduce(function (int $carry, int $item) {
+                return $carry + $item;
+            }, 0);
+
             return $this->successfulResponseWithCollection('API request successful', [
-                'total' => $characters->count(),
-                'total_height' => $total_height,
+                'total_characters' => $characters->count(),
+                'total_height_in_cm' => $total_height,
+                'total_height_in_ft' => cmToFeet($total_height),
                 'characters' => CharacterResource::collection($characters->values())
             ]);
         } catch (Exception $exception) {
